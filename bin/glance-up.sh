23fc9b884de82793d24393f44b7ec0e9a2be1ccd
Hugo Firth <me@hugofirth.com>
2017-11-06 15:50:03 +0000

    Added sudo to glance-up

--- bin/glance-up.sh
+++ bin/glance-up.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+sudo true
+
 # First we need to execute sbt-docker
-sbt dockerize
+sudo sbt dockerize
 
 # Then we need to create the pg-data directory to be mounted as a volume in docker up
 mkdir -p pg-data
 
 # Then we run docker compose up to launch the database container
-docker-compose up glance-eval-db
+sudo docker-compose up glance-eval-db
 
 # run flyway migrations
 eval "$(cat .env)"
-sbt -Dflyway.password=$GLANCE_DB_PASS -Dflyway.user=$GLANCE_DB_USER -Dflyway.url="jdbc:postgresql://localhost:8003/$(GLANCE_DB_NAME)" glance-coreJVM/flywayMigrate
+sudo sbt -Dflyway.password=$GLANCE_DB_PASS -Dflyway.user=$GLANCE_DB_USER -Dflyway.url="jdbc:postgresql://localhost:8003/$(GLANCE_DB_NAME)" glance-coreJVM/flywayMigrate
 
-docker-compose up
+sudo docker-compose up

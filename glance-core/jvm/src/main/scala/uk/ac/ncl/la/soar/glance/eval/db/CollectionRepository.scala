6496252508b58a4c967114c30aea34ee52b1a235
Hugo Firth <me@hugofirth.com>
2017-12-05 16:55:49 +0000

    Finished Collection Repository layer, working on generator

--- glance-core/jvm/src/main/scala/uk/ac/ncl/la/soar/glance/eval/db/CollectionRepository.scala
+++ glance-core/jvm/src/main/scala/uk/ac/ncl/la/soar/glance/eval/db/CollectionRepository.scala
@@ -40,24 +40,45 @@ class CollectionDb private[glance] (xa: Transactor[Task]) extends Repository[Col
   override val init = initQ.transact(xa)
   override val list = listQ.transact(xa)
 
-  override def find(id: UUID) = ???
+  override def find(id: UUID) = findQ(id).transact(xa)
 
-  override def save(entry: Collection) = ???
+  override def save(entry: Collection) = saveQ(entry).transact(xa)
 
-  override def delete(id: UUID) =  ???
+  override def delete(id: UUID) = deleteQ(id).transact(xa)
 }
 
 object CollectionDb extends RepositoryCompanion[Collection, CollectionDb] {
 
   type CollectionRow = (UUID, ModuleCode, Int)
   type CollectionMembership = (UUID, Int, Boolean)
+  type CollectionMembershipRow = (UUID, UUID, Int, Boolean)
 
   implicit val uuidMeta: Meta[UUID] = Meta[String].nxmap(UUID.fromString, _.toString)
 
-  override val initQ = ().pure[ConnectionIO]
-  override val listQ = List.empty[Collection].pure[ConnectionIO]
+  override val initQ: ConnectionIO[Unit] = ().pure[ConnectionIO]
+  override val listQ: ConnectionIO[List[Collection]] = {
+    type Row = (UUID, ModuleCode, Int, UUID, Int)
+    //TODO: Read up on process/stream api from fs2!
+    val rowsQ =
+      sql"""
+        SELECT c.id, c.module_num, c.num_entries, cm.survey_id, cm.membership_idx
+        FROM collection c, collection_membership cm
+        WHERE c.id == cm.collection_id;
+      """.query[Row].list
 
-  override def findQ(id: UUID) = {
+    rowsQ.map { rows: List[Row] =>
+      val collToMem = rows.groupBy(r => (r._1, r._2, r._3))
+      val memSorted = collToMem.collect { case (c, m) if m.nonEmpty => c -> m.sortBy(_._2).map(_._1) }
+      val collOpts = memSorted.flatMap {
+        case (c, hd :: tl) => Some(Collection(c._1, c._2, NonEmptyVector(hd, tl.toVector)))
+        case (c, Nil) => None
+      }
+      collOpts.toList
+    }
+  }
+
+  //TODO: Look at refactoring or factoring out this method and the above. One must be better than the other
+  override def findQ(id: UUID): ConnectionIO[Option[Collection]] = {
     for {
       cR <- findCollectionRowQ(id)
       cMR <- findCollectionMembershipsQ(id)
@@ -70,19 +91,50 @@ object CollectionDb extends RepositoryCompanion[Collection, CollectionDb] {
     }
   }
 
-  private def findCollectionRowQ(id: UUID): ConnectionIO[Option[CollectionRow]] =
+  private def findCollectionRowQ(id: UUID) =
     sql"""
       SELECT c.id, c.module_num, c.num_entries FROM collection c WHERE c.id = $id;
     """.query[CollectionRow].option
 
-  private def findCollectionMembershipsQ(id: UUID): ConnectionIO[List[CollectionMembership]] =
+  private def findCollectionMembershipsQ(id: UUID) =
     sql"""
-      SELECT c.collection_id, c.survey_id, c.membership_idx, c.last
+      SELECT c.survey_id, c.membership_idx, c.last
       FROM collection_membership c WHERE c.collection_id = $id;
     """.query[CollectionMembership].list
 
 
-  override def saveQ(entry: Collection) = ???
+  override def saveQ(entry: Collection): ConnectionIO[Unit] = {
 
-  override def deleteQ(id: UUID) = ???
+    //Batch insert entries into collection_membership table
+    val addMembershipsSQL =
+      """
+        INSERT INTO collection_membership (collection_id, survey_id, membership_idx, last)
+        VALUES (?, ?, ?, ?);
+      """
+
+    val addCollectionQ =
+      sql"""
+        INSERT INTO collection (id, module_num, num_entries)
+        VALUES (${entry.id}, ${entry.module}, ${entry.numEntries});
+      """.update.run
+
+    //Prepare membership rows
+    val surveyIndices = entry.surveyIds.toVector.zipWithIndex
+
+    //Add collection id and "last" flag
+    val last = entry.numEntries -1
+    val membershipRows = surveyIndices.map {
+      case (s, i) if i == last => (entry.id, s, i, true)
+      case (s, i) => (entry.id, s, i, false)
+    }
+
+    //Add all db rows
+    for {
+      _ <- addCollectionQ
+      _ <- Update[CollectionMembershipRow](addMembershipsSQL).updateMany(membershipRows)
+    } yield ()
+  }
+
+  override def deleteQ(id: UUID) =
+    sql"DELETE FROM collection c WHERE c.id = $id;".update.run.map(_ > 0)
 }
